cfd95b435c89bfc357128c284d4ccd7db6f973e7
patrick.bellositz <patrick.bellositz@192.168.2.2>
2016-06-21 13:17:53 +0200

    created other component modules/classes, removed main.py (not needed?)

---
+++ VU_PS_UE2/main/Guard.py
@@ -0,0 +1,14 @@
+'''
+Created on 21. Juni 2016
+
+@author: #put your name here
+'''
+import Component
+
+class Guard(Component):
+    
+    def __init__(self,input):
+        super().__init__(self,input)
+    
+    def checkSyntax(self):
+        return #something should go here
--- VU_PS_UE2/main/main.py
+++
@@ -1,5 +0,0 @@
-'''
-Created on 19. Juni 2016
-
-@author: patrick.bellositz
-'''
