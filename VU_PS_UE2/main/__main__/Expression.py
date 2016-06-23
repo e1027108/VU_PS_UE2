fed8c31d206af8f57a3ddf868383ba34ce1ae0c4
raidsnail <raidsnail@raidsnail_PC>
2016-06-23 19:15:16 +0200

    added expression and guard

--- VU_PS_UE2/main/__main__/Expression.py
+++ VU_PS_UE2/main/__main__/Expression.py
@@ -1,9 +1,10 @@
 '''
 Created on 21. Juni 2016
 
-@author: #put your name here
+@author: raidsnail
 '''
 from __main__ import Component
+from __main__ import Block
 
 class Expression(Component):
     
@@ -11,4 +12,53 @@ class Expression(Component):
         super().__init__(self,input)
     
     def checkSyntax(self):
-        return #something should go here
+        
+        test = self.input.lstrip()
+        test = test.rstrip()
+        
+        nameIndex = 0
+        expIndex = 0
+        
+        part1 = test
+        
+        for x in range(0, len(test)):
+            if (x =='.'):
+                # this is a name starting with .
+                if(nameIndex == 0):
+                    part1 = test[:x-1]
+                nameIndex = x+1;
+            elif(x == '+' and expIndex == 0):
+                # optional expression
+                e = Expression(test[x+1:])
+                if(nameIndex == 0):
+                    part1 = test[:x-1]
+                expIndex = x+1;
+                
+        
+
+        if (expIndex == 0):
+            return (self.checkPart1(test))     
+        else:
+            return (self.checkPart1(part1) and e.checkSyntax())
+        
+        return False
+    
+        ## missing .name and *name handling!!!
+
+        
+    def checkPart1(self, test):
+        
+        if (test[0] == '"' and test[len(test)-1]):
+            return self.checkStringLiteral(test)
+        elif (test[0] == '{' and test[len(test)-1] == '}'):
+            b = Block(test[1:len(test)-1])
+            return b.checkSyntax()
+        elif (test[0] == '(' and test[len(test)-1] == ')'):
+            e = Expression(test[1:len(test)-1])
+            return e.checkSyntax()
+        else:
+            # this is a name with none or multiple *
+            pass
+        
+        return False
+        
--- VU_PS_UE2/main/__main__/Guard.py
+++ VU_PS_UE2/main/__main__/Guard.py
@@ -1,9 +1,10 @@
 '''
 Created on 21. Juni 2016
 
-@author: #put your name here
+@author: raidsnail
 '''
 from __main__ import Component
+from __main__ import Expression
 
 class Guard(Component):
     
@@ -11,4 +12,32 @@ class Guard(Component):
         super().__init__(self,input)
     
     def checkSyntax(self):
-        return #something should go here
+        
+        test = self.input.lstrip()
+        test = test.rstrip()
+             
+        expIndex = 0
+        guardIndex = 0
+        
+               
+        for x in range(0, len(str)):
+            if ((x == '=') or (x == '#')):
+                e1 = Expression(test[:x-1])
+                expIndex = x+1
+            elif (x == ','):
+                e2 = Expression(test[expIndex:x-1])
+                guardIndex = x+1
+                
+        if(expIndex == 0):
+            return False
+                
+        if(guardIndex == 0):
+            e2 = Expression(test[expIndex:])
+            return (e1.checkSyntax() and e2.checkSyntax())
+        else:
+            g = Guard(test[guardIndex:])
+            return (e1.checkSyntax() and e2.checkSyntax() and g.checkSyntax())
+        
+        
+        
+        
