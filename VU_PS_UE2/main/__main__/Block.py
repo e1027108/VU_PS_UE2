9683eba040047cd3711f83c43c1534a3fdbb66c1
patrick.bellositz <patrick.bellositz@192.168.2.6>
2016-07-20 14:58:33 +0200

    first attempt at implementing block

--- VU_PS_UE2/main/__main__/Block.py
+++ VU_PS_UE2/main/__main__/Block.py
@@ -17,7 +17,15 @@ class Block(Component):
         
         if (test[0] == '{') and (test[len(test)-1] == '}'):
             c = Command(test[1:len(test)-1]) #supposed to put though everything between {}
-            return c.checkSyntax()
+            c.setPropertyList(self.property_list) #commands should get access to all already existing propertylists?
+            if c.checkSyntax():
+                cList = c.getPropertyList().getDict()
+                sList = self.property_list.getDict()
+                for prop in cList:
+                    sList.add(prop)
+                return True
+            else:
+                return False
         else:
             print "The first symbol (currently '" + test[0] + ("') needs to be '{' and the last symbol "
                 "(currently '") + test[len(test)-1] + "') needs to be '}'. Therefore the block is not valid."
